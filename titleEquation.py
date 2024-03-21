cd09c7fb5bf5bcab9f19c5d2c79915e67cb7ca08
ricardo <nocive.science@gmail.com>
2024-03-21 01:59:20 -0300

    terminado

--- titleEquation.py
+++ titleEquation.py
@@ -2,6 +2,6 @@ from manim import *
 import itertools
 class TituloScene(Scene):
     def construct(self):
-        titulo=Text("Ecuaciones diferenciales" ,t2c={"diferenciales":RED,"Ecuaciones":BLUE})
+        titulo=Text("Ecuaciones diferenciales parciales" ,t2c={"diferenciales":RED,"Ecuaciones":BLUE, "parciales":YELLOW}, font="NOTO SERIF", weight="BOLD").set(width=config["frame_width"]-1).to_edge(UP)
         self.play(Write(titulo))
         self.wait(10)
